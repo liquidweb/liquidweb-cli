2ee1f75b8a7d69c02cec67cb13b73caa918f5342
Scott Sullivan <scottgregorysullivan@gmail.com>
2020-01-06 16:03:25 -0500

    add 'cloud storage block details'

---
+++ cmd/cloudStorageBlockDetails.go
@@ -0,0 +1,68 @@
+/*
+Copyright © LiquidWeb
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"github.com/liquidweb/liquidweb-cli/types/api"
+)
+
+var cloudStorageBlockDetailsCmd = &cobra.Command{
+	Use:   "details",
+	Short: "Get details of a Cloud Block Storage volume",
+	Long: `Get details of a Cloud Block Storage volume.
+
+Block storage offers a method to attach additional storage to Cloud Server.
+Once attached, volumes appear as normal block devices, and can be used as such.
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		uniqIdFlag, _ := cmd.Flags().GetString("uniq_id")
+		jsonFlag, _ := cmd.Flags().GetBool("json")
+
+		if uniqIdFlag == "" {
+			lwCliInst.Die(fmt.Errorf("--uniq_id is a required flag"))
+		}
+
+		apiArgs := map[string]interface{}{"uniq_id": uniqIdFlag}
+
+		var details apiTypes.CloudBlockStorageVolumeDetails
+		err := lwCliInst.CallLwApiInto("bleed/storage/block/volume/details", apiArgs, &details)
+		if err != nil {
+			lwCliInst.Die(err)
+		}
+
+		if jsonFlag {
+			pretty, err := lwCliInst.JsonEncodeAndPrettyPrint(details)
+			if err != nil {
+				lwCliInst.Die(err)
+			}
+			fmt.Printf(pretty)
+		} else {
+			_printCloudBlockStorageVolumeDetailsStruct(&details)
+
+		}
+	},
+}
+
+func init() {
+	cloudStorageBlockCmd.AddCommand(cloudStorageBlockDetailsCmd)
+
+	cloudStorageBlockDetailsCmd.Flags().Bool("json", false, "output in json format")
+	cloudStorageBlockDetailsCmd.Flags().String("uniq_id", "", "uniq_id of Cloud Block Storage volume")
+}
