8d0d5537fb0195a8805a6d08bfa80a873eafcd31
Scott Sullivan <scottgregorysullivan@gmail.com>
2020-01-06 16:09:01 -0500

    update wording

--- cmd/root.go
+++ cmd/root.go
@@ -39,9 +39,11 @@ var rootCmd = &cobra.Command{
 Command line interface for interacting with LiquidWeb services via
 LiquidWebs Public API.
 
-Your public API credentials must be in the liquidweb-cli config
-file. By default, this will be $HOME/.liquidweb-cli.yaml. This can
-be overrode with the config flag.
+If this is your first time running, you will need to setup auth
+contexts. An auth context contains authenication data for accessing
+your LiquidWeb Account. To setup your first auth context, you can
+run 'auth init'. For further information on auth contexts, be sure
+to checkout 'help auth' for a list of capabilities.
 
 Consult the various subcommands for specific features and
 capabilities.`,
