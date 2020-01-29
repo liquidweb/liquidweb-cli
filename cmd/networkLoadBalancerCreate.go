561bfdb4a71b61a0b305d97cb3f31c218d5f79a7
Scott Sullivan <scottgregorysullivan@gmail.com>
2020-01-29 08:42:34 -0500

    add 'network load-balancer create'

--- cmd/networkLoadBalancerCreate.go
+++ cmd/networkLoadBalancerCreate.go
@@ -27,15 +27,14 @@ import (
 	"github.com/liquidweb/liquidweb-cli/validate"
 )
 
-var networkLoadBalancerUpdateNodesCmd []string
-var networkLoadBalancerUpdateServicesCmd []string
+var networkLoadBalancerCreateNodesCmd []string
+var networkLoadBalancerCreateServicesCmd []string
 
-var networkLoadBalancerUpdateCmd = &cobra.Command{
-	Use:   "update",
-	Short: "Change configuration of an existing Load Balancer",
-	Long:  `Change configuration of an existing Load Balancer`,
+var networkLoadBalancerCreateCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Create a Load Balancer",
+	Long:  `Create a Load Balancer`,
 	Run: func(cmd *cobra.Command, args []string) {
-		uniqIdFlag, _ := cmd.Flags().GetString("uniq_id")
 		nameFlag, _ := cmd.Flags().GetString("name")
 		strategyFlag, _ := cmd.Flags().GetString("strategy")
 		enableSslTerminationFlag, _ := cmd.Flags().GetBool("enable-ssl-termination")
@@ -45,6 +44,7 @@ var networkLoadBalancerUpdateCmd = &cobra.Command{
 		sslIntermediateCertFlag, _ := cmd.Flags().GetString("ssl-intermediate-certificate")
 		enableSslIncludesFlag, _ := cmd.Flags().GetBool("enable-ssl-includes")
 		disableSslIncludesFlag, _ := cmd.Flags().GetBool("disable-ssl-includes")
+		regionFlag, _ := cmd.Flags().GetInt("region")
 
 		if enableSslTerminationFlag && disableSslTerminationFlag {
 			lwCliInst.Die(fmt.Errorf("can't both enable and disable ssl termination"))
@@ -64,11 +64,18 @@ var networkLoadBalancerUpdateCmd = &cobra.Command{
 		}
 
 		validateFields := map[interface{}]interface{}{
-			uniqIdFlag: "UniqId",
+			strategyFlag: "LoadBalancerStrategy",
+			nameFlag:     "NonEmptyString",
 		}
 
 		apiArgs := map[string]interface{}{
-			"uniq_id": uniqIdFlag,
+			"name":     nameFlag,
+			"strategy": strategyFlag,
+		}
+
+		if regionFlag != 0 {
+			validateFields[regionFlag] = "PositiveInt"
+			apiArgs["region"] = regionFlag
 		}
 
 		// ssl termination
@@ -77,17 +84,6 @@ var networkLoadBalancerUpdateCmd = &cobra.Command{
 		}
 		if disableSslTerminationFlag {
 			apiArgs["ssl_termination"] = false
-		}
-
-		// name
-		if nameFlag != "" {
-			apiArgs["name"] = nameFlag
-		}
-
-		// strategy
-		if strategyFlag != "" {
-			validateFields[strategyFlag] = "LoadBalancerStrategy"
-			apiArgs["strategy"] = strategyFlag
 		}
 
 		// ssl includes
@@ -133,40 +129,37 @@ var networkLoadBalancerUpdateCmd = &cobra.Command{
 		}
 
 		// nodes
-		if len(networkLoadBalancerUpdateNodesCmd) > 0 {
-			apiArgs["nodes"] = networkLoadBalancerUpdateNodesCmd
-			for _, ip := range networkLoadBalancerUpdateNodesCmd {
+		if len(networkLoadBalancerCreateNodesCmd) > 0 {
+			apiArgs["nodes"] = networkLoadBalancerCreateNodesCmd
+			for _, ip := range networkLoadBalancerCreateNodesCmd {
 				validateFields[ip] = "IP"
 			}
 		}
 
 		// services
-		if len(networkLoadBalancerUpdateServicesCmd) > 0 {
-			// slice of maps with keys src_port, dest_port, with a value of its network port number.
-			var servicesToBalance []map[string]int
+		if len(networkLoadBalancerCreateServicesCmd) == 0 {
+			lwCliInst.Die(fmt.Errorf("--services must have source/destination port pairs (see 'help network load-balancer create')"))
+		}
+		// slice of maps with keys src_port, dest_port, with a value of its network port number.
+		var servicesToBalance []map[string]int
 
-			for _, pair := range networkLoadBalancerUpdateServicesCmd {
-				err := validate.Validate(map[interface{}]interface{}{pair: "NetworkPortPair"})
-				if err != nil {
-					lwCliInst.Die(err)
-				}
-
-				splitPair := strings.Split(pair, ":")
-				srcPort := cast.ToInt(splitPair[0])
-				destPort := cast.ToInt(splitPair[1])
-
-				servicesToBalance = append(servicesToBalance, map[string]int{
-					"src_port":  srcPort,
-					"dest_port": destPort,
-				})
+		for _, pair := range networkLoadBalancerCreateServicesCmd {
+			err := validate.Validate(map[interface{}]interface{}{pair: "NetworkPortPair"})
+			if err != nil {
+				lwCliInst.Die(err)
 			}
 
-			apiArgs["services"] = servicesToBalance
+			splitPair := strings.Split(pair, ":")
+			srcPort := cast.ToInt(splitPair[0])
+			destPort := cast.ToInt(splitPair[1])
+
+			servicesToBalance = append(servicesToBalance, map[string]int{
+				"src_port":  srcPort,
+				"dest_port": destPort,
+			})
 		}
 
-		if len(apiArgs) == 1 {
-			lwCliInst.Die(fmt.Errorf("Must pass something to update. See 'help network load-balancer update'"))
-		}
+		apiArgs["services"] = servicesToBalance
 
 		// validate built input
 		if err := validate.Validate(validateFields); err != nil {
@@ -174,34 +167,36 @@ var networkLoadBalancerUpdateCmd = &cobra.Command{
 		}
 
 		// call the method, display results
-		var update apiTypes.NetworkLoadBalancerDetails
-		if err := lwCliInst.CallLwApiInto("bleed/network/loadbalancer/update", apiArgs,
-			&update); err != nil {
+		var create apiTypes.NetworkLoadBalancerDetails
+		if err := lwCliInst.CallLwApiInto("bleed/network/loadbalancer/create", apiArgs,
+			&create); err != nil {
 			lwCliInst.Die(err)
 		}
 
-		fmt.Print(update)
+		fmt.Print(create)
 	},
 }
 
 func init() {
-	networkLoadBalancerCmd.AddCommand(networkLoadBalancerUpdateCmd)
-	networkLoadBalancerUpdateCmd.Flags().String("uniq_id", "", "uniq_id of Load Balancer")
-	networkLoadBalancerUpdateCmd.Flags().String("strategy", "", "Load Balancer strategy (see 'network load-balancer get-strategies')")
-	networkLoadBalancerUpdateCmd.Flags().String("name", "", "name of Load Balancer")
-	networkLoadBalancerUpdateCmd.Flags().Bool("enable-ssl-termination", false, "enable ssl termination")
-	networkLoadBalancerUpdateCmd.Flags().Bool("disable-ssl-termination", false, "disable ssl termination")
-	networkLoadBalancerUpdateCmd.Flags().String("ssl-private-key", "", "path to ssl private key")
-	networkLoadBalancerUpdateCmd.Flags().String("ssl-certificate", "", "path to ssl certificate")
-	networkLoadBalancerUpdateCmd.Flags().String("ssl-intermediate-certificate", "", "path to ssl ssl intermediate certificate")
-	networkLoadBalancerUpdateCmd.Flags().Bool("enable-ssl-includes", false, "enable ssl includes")
-	networkLoadBalancerUpdateCmd.Flags().Bool("disable-ssl-includes", false, "disable ssl includes")
+	networkLoadBalancerCmd.AddCommand(networkLoadBalancerCreateCmd)
+	networkLoadBalancerCreateCmd.Flags().String("strategy", "", "Load Balancer strategy (see 'network load-balancer get-strategies')")
+	networkLoadBalancerCreateCmd.Flags().String("name", "", "name of Load Balancer")
+	networkLoadBalancerCreateCmd.Flags().Bool("enable-ssl-termination", false, "enable ssl termination")
+	networkLoadBalancerCreateCmd.Flags().Bool("disable-ssl-termination", false, "disable ssl termination")
+	networkLoadBalancerCreateCmd.Flags().String("ssl-private-key", "", "path to ssl private key")
+	networkLoadBalancerCreateCmd.Flags().String("ssl-certificate", "", "path to ssl certificate")
+	networkLoadBalancerCreateCmd.Flags().String("ssl-intermediate-certificate", "", "path to ssl ssl intermediate certificate")
+	networkLoadBalancerCreateCmd.Flags().Bool("enable-ssl-includes", false, "enable ssl includes")
+	networkLoadBalancerCreateCmd.Flags().Bool("disable-ssl-includes", false, "disable ssl includes")
+	networkLoadBalancerCreateCmd.Flags().Int("region", 0, "region id to create a Load Balancer in (see 'cloud server options --zones')")
 
-	networkLoadBalancerUpdateCmd.Flags().StringSliceVar(&networkLoadBalancerUpdateNodesCmd, "nodes",
+	networkLoadBalancerCreateCmd.Flags().StringSliceVar(&networkLoadBalancerCreateNodesCmd, "nodes",
 		[]string{}, "nodes (ips) separated by ',' to balance via the Load Balancer (see 'network load-balancer get-possible-nodes')")
 
-	networkLoadBalancerUpdateCmd.Flags().StringSliceVar(&networkLoadBalancerUpdateServicesCmd, "services",
+	networkLoadBalancerCreateCmd.Flags().StringSliceVar(&networkLoadBalancerCreateServicesCmd, "services",
 		[]string{}, "source/destination port pairs (such as 80:80) separated by ',' to balance via the Load Balancer")
 
-	networkLoadBalancerUpdateCmd.MarkFlagRequired("uniq_id")
+	networkLoadBalancerCreateCmd.MarkFlagRequired("name")
+	networkLoadBalancerCreateCmd.MarkFlagRequired("services")
+	networkLoadBalancerCreateCmd.MarkFlagRequired("strategy")
 }
