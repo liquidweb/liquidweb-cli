8a806c4d194ea62babb627d3dafda8844d9d6327
Jeff VanDellen <jvandellen@liquidweb.com>
2023-09-29 13:46:48 -0400

    Fixed golinter issues from dependabot

--- cmd/authAddContext.go
+++ cmd/authAddContext.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,6 +18,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/spf13/cobra"
@@ -46,7 +47,7 @@ Use this if you've already setup contexts with "auth init".`,
 			lwCliInst.Die(err)
 		}
 		if !utils.FileExists(file) {
-			f, err := os.Create(file)
+			f, err := os.Create(filepath.Clean(file))
 			if err != nil {
 				lwCliInst.Die(err)
 			}
